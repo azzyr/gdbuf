d63f530a07ece0f55ceb5d5f9ad0fa5be5c1f12b
azzyr <azzyr@protonmail.com>
2026-07-17 15:19:49 +0300

    feat: Remove unused triplets folder??

--- internal/gdextension/buildenv/cmake/arm-linux.toolchain.cmake
+++
@@ -1,8 +0,0 @@
-# The name of the target operating system.
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
-
-# Specify the cross-compilers.
-# The GitHub Actions workflow installs 'g++-arm-linux-gnueabihf' which provides these.
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
